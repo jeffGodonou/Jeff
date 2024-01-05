1bef09aa1e81a3208402aa76d559618bb693ac25
jeffGodonou <jeff1949@hotmail.fr>
2024-01-05 13:33:10 -0500

    action get_weather correction (1)

--- actions/actions.py
+++ actions/actions.py
@@ -41,9 +41,8 @@ class ActionGetWeather(Action):
     ) -> List[Dict[Text, Any]]:
         # Get location from the tracker
         location = tracker.get_slot("location")
-        
-        #dispatcher.utter_message("The weather in {} on {} is sunny.".format(location))
-        tracker.set_slot("requested_weather", True)
+        # dispatcher.utter_message("The weather in {} on {} is sunny.".format(location))
+        # tracker.set_slot("requested_weather", True)
 
         # OpenWeatherMap API key
         api_key = '7f88e0b55a9fa291a1b2854af083a0e3'
